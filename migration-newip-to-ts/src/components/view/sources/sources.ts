983ed06b7171e02677375f1f1e7e3917c2b9173a
Dauhaliavets <dolgolevets21@gmail.com>
2022-07-04 01:55:38 +0300

    feat: add pagination for sources

--- migration-newip-to-ts/src/components/view/sources/sources.ts
+++ migration-newip-to-ts/src/components/view/sources/sources.ts
@@ -2,20 +2,61 @@ import './sources.css';
 import { DataSources } from './sources.types';
 
 class Sources {
-  draw(data: DataSources[]) {
-    const fragment = document.createDocumentFragment();
+  private currentPage: number = 1;
+  readonly itemsOnPage: number = 6;
+  private countPages: number = 0;
+
+  draw(data: DataSources[]): void {
+    this.countPages = Math.ceil(data.length / this.itemsOnPage);
+    this.drawSources(data, this.currentPage);
+    this.setPagination(data);
+    this.updatePagination(this.currentPage);
+  }
+
+  private drawSources(sources: DataSources[], pageNumber: number): void {
+    let startIndex: number = this.itemsOnPage * (pageNumber - 1);
+    let endIndex: number = this.itemsOnPage * pageNumber;
+
     const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
 
-    data.forEach((item) => {
-      const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
+    const visibleSources = sources
+      .slice(startIndex, endIndex)
+      .map((item: DataSources) => this.createSourceItem(item, sourceItemTemp));
 
-      (sourceClone.querySelector('.source__item-name') as HTMLElement).textContent = item.name;
-      (sourceClone.querySelector('.source__item') as HTMLElement).setAttribute('data-source-id', item.id);
+    const sourcesContainer = document.querySelector('.sources') as HTMLElement;
+    sourcesContainer.innerHTML = '';
+    sourcesContainer.append(...visibleSources);
+  }
 
-      fragment.append(sourceClone);
+  private createSourceItem(item: DataSources, template: HTMLTemplateElement): HTMLElement {
+    const sourceClone = template.content.cloneNode(true) as HTMLElement;
+
+    (sourceClone.querySelector('.source__item-name') as HTMLElement).textContent = item.name;
+    (sourceClone.querySelector('.source__item') as HTMLElement).setAttribute('data-source-id', item.id);
+
+    return sourceClone;
+  }
+
+  private setPagination(sources: DataSources[]): void {
+    (document.querySelector('.prev-page') as HTMLElement).addEventListener('click', () => {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+        this.drawSources(sources, this.currentPage);
+        this.updatePagination(this.currentPage);
+      }
     });
 
-    (document.querySelector('.sources') as HTMLElement).append(fragment);
+    (document.querySelector('.next-page') as HTMLElement).addEventListener('click', () => {
+      if (this.currentPage < this.countPages) {
+        this.currentPage++;
+        this.drawSources(sources, this.currentPage);
+        this.updatePagination(this.currentPage);
+      }
+    });
+  }
+
+  private updatePagination(pageNumber: number): void {
+    (document.querySelector('.current-page') as HTMLElement).textContent = `${pageNumber} / ${this.countPages}`;
   }
 }
 
