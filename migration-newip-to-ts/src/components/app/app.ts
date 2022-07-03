a266428efb2ffe1240395eb862c259e847de02ad
Dauhaliavets <dolgolevets21@gmail.com>
2022-07-03 22:35:45 +0300

    feat: add types for app, rename index

---
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -0,0 +1,22 @@
+import { NewsResponse, SourcesResponse } from './../view/appView.types';
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+
+class App {
+  private controller: AppController;
+  private view: AppView;
+
+  constructor() {
+    this.controller = new AppController();
+    this.view = new AppView();
+  }
+
+  start(): void {
+    (document.querySelector('.sources') as HTMLElement).addEventListener('click', (e: Event) =>
+      this.controller.getNews(e, (data: NewsResponse): void => this.view.drawNews(data)),
+    );
+    this.controller.getSources((data: SourcesResponse): void => this.view.drawSources(data));
+  }
+}
+
+export default App;
