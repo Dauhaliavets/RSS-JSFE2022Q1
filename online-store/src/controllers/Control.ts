884b2cc0d1b2c9781efab1877d10cea67d13ac05
Dauhaliavets <dolgolevets21@gmail.com>
2022-07-12 23:59:43 +0300

    feat: add base component Control, Signal

---
+++ online-store/src/controllers/Control.ts
@@ -0,0 +1,17 @@
+class Control<NodeType extends HTMLElement> {
+  public node: NodeType;
+
+  constructor(parentElement: HTMLElement, tagName: string = 'div', classList = '', content = '') {
+    const element = document.createElement(tagName);
+    element.className = classList;
+    element.textContent = content;
+
+    if (parentElement) {
+      parentElement.append(element);
+    }
+
+    this.node = element as NodeType;
+  }
+}
+
+export { Control };
