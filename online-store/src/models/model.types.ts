b4399c819039204101897e9049175273441f33fd
Dauhaliavets <dolgolevets21@gmail.com>
2022-07-13 16:29:22 +0300

    refactor: refactor data model

--- online-store/src/models/model.types.ts
+++ online-store/src/models/model.types.ts
@@ -1,20 +1,31 @@
 type ProductDescription = {
   [index: string]: string;
+};
+
+export enum Category {
+  Laptop = 'Laptop',
+  Monoblock = 'Monoblock',
+  Printer = 'Printer',
+  Smartphone = 'Smartphone',
+  Tv = 'TV',
 }
 
 export interface Product {
-    id: number,
-    brand: string,
-    category: string,
-    title: string,
-    description: ProductDescription,
-    price: number,
-    img: string,
-    count: number,
+  id: number;
+  brand: string;
+  category: Category;
+  title: string;
+  description: ProductDescription;
+  price: number;
+  discount: number;
+  currency: string;
+  img: string;
+  isChoise: boolean;
+  count: number;
 }
 
 export interface AppState {
-  products: Product[],
-  cart: Product[],
-  isOnCart: boolean,
-} 
+  products: Array<Product>;
+  cart: Array<Product>;
+  isOnCart: boolean;
+}
