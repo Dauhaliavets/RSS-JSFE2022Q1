01d300a070e167e9f42ec3e1be17701c2c5a2691
Dauhaliavets <dolgolevets21@gmail.com>
2022-07-18 12:25:19 +0300

    docs: add task self-check

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -2,6 +2,7 @@ import { Controller } from './controllers/controller';
 import { Model } from './models/model';
 import { View } from './views/view';
 import './styles.scss';
+import './taskCheck';
 
 const model = new Model();
 const controller = new Controller(model);
